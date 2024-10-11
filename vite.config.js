e34da9f6a2aa83c78c3223c9628db2c2de1ce046
felixa1243 <felixarajiph@gmail.com>
2024-10-11 16:20:19 +0700

    test vite

--- vite.config.js
+++ vite.config.js
@@ -11,9 +11,7 @@ export default defineConfig({
   },
   css: {
     preprocessorOptions: {
-      scss: {
-        additionalData: `@import "./css/style.scss";`,
-      },
+      scss: {},
     },
   },
   resolve: {
